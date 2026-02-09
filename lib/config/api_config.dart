2f7c500d5ec3552111d77b733335d43ee6872234
Doane Marie Horlador <doanemarie.horlador@wvsu.edu.ph>
2026-02-09 10:35:17 +0800

    Updated On Friday

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,6 +2,7 @@ class ApiConfig {
   // Update this to your actual backend URL
   static const String baseUrl = 'http://10.0.2.2:8082';
   // static const String baseUrl = 'http://localhost:8082';
+  //Hello Dianna
   
   static const String loginEndpoint = '/auth/login';
   static const String getUserEndpoint = '/adminuser/get-profile';
